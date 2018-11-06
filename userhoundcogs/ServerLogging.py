635858c0afdae1735ed84e06e87ea8b282fd2b92
Deivedux <36776659+Deivedux@users.noreply.github.com>
2018-11-06 16:09:20 +0200

    Update ServerLogging

--- userhoundcogs/ServerLogging.py
+++ userhoundcogs/ServerLogging.py
@@ -26,7 +26,7 @@ class ServerLogging:
 			channel = self.bot.get_channel(logging_channels[message.guild.id])
 			if channel != None:
 				embed = discord.Embed(title = '<:messagedelete:439643744833241101> Message Deleted in #' + message.channel.name, description = message.content, color = 0xD4AC0D, timestamp = datetime.datetime.utcnow())
-				embed.set_author(name = str(message.author), icon_url = message.author.avatar_url)
+				embed.set_footer(text = str(message.author), icon_url = message.author.avatar_url)
 				await channel.send(embed = embed)
 			else:
 				c.execute("DELETE FROM ServerLogging WHERE Guild = " + str(message.guild.id))
@@ -47,7 +47,7 @@ class ServerLogging:
 			channel = self.bot.get_channel(logging_channels[after.guild.id])
 			if channel != None:
 				embed = discord.Embed(title = '<:messageupdate:439643744849887233> Message Edited in #' + after.channel.name, color = 0x2471A3, timestamp = datetime.datetime.utcnow())
-				embed.set_author(name = str(after.author), icon_url = after.author.avatar_url)
+				embed.set_footer(text = str(after.author), icon_url = after.author.avatar_url)
 				embed.add_field(name = 'Before', value = before.content, inline = False)
 				embed.add_field(name = 'After', value = after.content)
 				await channel.send(embed = embed)
@@ -60,7 +60,8 @@ class ServerLogging:
 		if member.guild.id in logging_channels.keys():
 			channel = self.bot.get_channel(logging_channels[member.guild.id])
 			if channel != None:
-				embed = discord.Embed(title = '<:memberjoin:439643745089224704> Member Joined', description = member.mention + ' (' + str(member) + ')\n\nThere are now ' + str(len(member.guild.members)) + ' members.', color = 0x28B463, timestamp = datetime.datetime.utcnow())
+				embed = discord.Embed(title = '<:memberjoin:439643745089224704> Member Joined', description = 'There are now ' + str(member.guild.member_count) + ' members.', color = 0x28B463, timestamp = datetime.datetime.utcnow())
+				embed.set_footer(text = str(member), icon_url = member.avatar_url)
 				await channel.send(embed = embed)
 			else:
 				c.execute("DELETE FROM ServerLogging WHERE Guild = " + str(member.guild.id))
@@ -71,7 +72,8 @@ class ServerLogging:
 		if member.guild.id in logging_channels.keys():
 			channel = self.bot.get_channel(logging_channels[member.guild.id])
 			if channel != None:
-				embed = discord.Embed(title = '<:memberleave:439643745059733514> Member Left', description = member.mention + ' (' + str(member) + ')\n\nThere are now ' + str(len(member.guild.members)) + ' members.', color = 0xCB4335, timestamp = datetime.datetime.utcnow())
+				embed = discord.Embed(title = '<:memberleave:439643745059733514> Member Left', description = 'There are now ' + str(member.guild.member_count) + ' members.', color = 0xCB4335, timestamp = datetime.datetime.utcnow())
+				embed.set_footer(text = str(member), icon_url = member.avatar_url)
 				await channel.send(embed = embed)
 			else:
 				c.execute("DELETE FROM ServerLogging WHERE Guild = " + str(member.guild.id))
@@ -84,7 +86,7 @@ class ServerLogging:
 			if channel != None:
 				if str(before.nick) != str(after.nick):
 					embed = discord.Embed(title = '<:memberupdate:439643745059733504> Nickname Updated', color = 0x7D3C98, timestamp = datetime.datetime.utcnow())
-					embed.set_author(name = str(after), icon_url = after.avatar_url)
+					embed.set_footer(text = str(after), icon_url = after.avatar_url)
 					embed.add_field(name = 'Before', value = str(before.nick))
 					embed.add_field(name = 'After', value = str(after.nick))
 					await channel.send(embed = embed)
@@ -98,7 +100,7 @@ class ServerLogging:
 			channel = self.bot.get_channel(logging_channels[guild.id])
 			if channel != None:
 				embed = discord.Embed(title = '<:bancreate:439643744816332810> Member Banned', color = 0xCB4335, timestamp = datetime.datetime.utcnow())
-				embed.set_author(name = str(member), icon_url = member.avatar_url)
+				embed.set_footer(text = str(member), icon_url = member.avatar_url)
 				await channel.send(embed = embed)
 			else:
 				c.execute("DELETE FROM ServerLogging WHERE Guild = " + str(guild.id))
@@ -110,7 +112,7 @@ class ServerLogging:
 			channel = self.bot.get_channel(logging_channels[guild.id])
 			if channel != None:
 				embed = discord.Embed(title = '<:bandelete:439643744803880960> Member Unbanned', color = 0x28B463, timestamp = datetime.datetime.utcnow())
-				embed.set_author(name = str(member), icon_url = member.avatar_url)
+				embed.set_footer(text = str(member), icon_url = member.avatar_url)
 				await channel.send(embed = embed)
 			else:
 				c.execute("DELETE FROM ServerLogging WHERE Guild = " + str(guild.id))
